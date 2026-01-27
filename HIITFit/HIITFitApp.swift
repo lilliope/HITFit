35bce2aee5a968c6e90458110e68ec569f3cf5bc
Lily Ton <loi.nguyen1099@gmail.com>
2026-01-26 22:52:12 -0500

    Initial Commit

---
+++ HIITFit/HIITFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  HIITFitApp.swift
+//  HIITFit
+//
+//  Created by Loi Ton on 1/26/26.
+//
+
+import SwiftUI
+
+@main
+struct HIITFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
