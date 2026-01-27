d5601b6db87cac607f900f8c605fb5c64ea5a297
Lily Ton <loi.nguyen1099@gmail.com>
2026-01-27 00:55:19 -0500

    Up to chapter 3

---
+++ HIITFit/Views/ExerciseView.swift
@@ -0,0 +1,37 @@
+//  ExerciseView.swift
+//  HIITFit
+//  Created by Loi Ton on 1/26/26.
+
+import SwiftUI
+
+struct ExerciseView: View {
+    let videoNames = ["squat", "step-up", "burpee", "sun-salute"]
+    let exerciseNames = ["Squat", "Step Up", "Burpee", "Sun Salute"]
+    let index: Int
+    var body: some View {
+        VStack {
+            VStack {
+                Text(exerciseNames[index])
+                    .font(.largeTitle)
+                HStack {
+                    Image(systemName: "1.circle")
+                    Image(systemName: "2.circle")
+                    Image(systemName: "3.circle")
+                    Image(systemName: "4.circle")
+                }
+                .font(.title2)
+            }
+            Text("Video player")
+            Text("Timer")
+            Text("Start/Done button")
+            Text("Rating")
+            Text("History button")
+        }
+    }
+}
+
+struct ExerciseView_Previews: PreviewProvider {
+    static var previews: some View {
+        ExerciseView(index: 0)
+    }
+}
