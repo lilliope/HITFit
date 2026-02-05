d7218eff150f4b5699c0a867aa8e593e094d1015
Lily Ton <loi.nguyen1099@gmail.com>
2026-02-04 23:44:19 -0500

    Chapter 8

--- HIITFit/HIITFitApp.swift
+++ HIITFit/HIITFitApp.swift
@@ -6,12 +6,28 @@ import SwiftUI
 
 @main
 struct HIITFitApp: App {
+    @StateObject private var historyStore = HistoryStore()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .onAppear {
                     print(URL.documentsDirectory)
                 }
+                .environmentObject(historyStore)
+                .alert(
+                    "History",
+                    isPresented: $historyStore.loadingError) {
+                        Button("OK") { }
+                    }
+                    message: {
+                        Text(
+                            """
+                            Unfortunately we can't load your past history.
+                            Email support:
+                            support@xyz.com
+                            """)
+                    }
         }
     }
 }
--- HIITFit/Views/ContentView.swift
+++ HIITFit/Views/ContentView.swift
@@ -16,9 +16,7 @@ struct ContentView: View {
                 .tag(index)
             }
         }
-        .environmentObject(HistoryStore())
         .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
-        
     }
 }
 
